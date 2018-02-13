6811f3d1b6bdfa5421e7d3d6286312f94bba9ba3
peejayibm <36212020+peejayibm@users.noreply.github.com>
2018-02-13 16:24:34 +1100

    Update iib_manage.sh

--- iib_manage.sh
+++ iib_manage.sh
@@ -171,9 +171,9 @@ monitor()
 	echo "Running - stop container to exit."
 	# Loop forever by default - container must be stopped manually.
   # Here is where you can add in conditions controlling when your container will exit - e.g. check for existence of specific processes stopping or errors being reported
-	# while true; do
-	#	sleep 1
-	# done
+	 while true; do
+		sleep 1
+	 done
 }
 mq-license-check.sh
 parameterCheck
